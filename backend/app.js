a938b37debabe2a25bf8213237936cff863d65e0
vectorsketcher <westboarderm@gmail.com>
2019-01-22 15:02:13 -0700

    hooked up mongo db, mongoose, has a cors issue for delete only

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const Post = require('./models/post');
 
 const app = express();
 
+//connection string to mongodatabase, uses mongoose for connection
 mongoose.connect("mongodb://admin:LigLiIO3sBVL94zI@cluster0-shard-00-00-h8mtn.mongodb.net:27017,cluster0-shard-00-01-h8mtn.mongodb.net:27017,cluster0-shard-00-02-h8mtn.mongodb.net:27017/node-angular?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true")
 .then(() => {
   console.log("Connected to Mongo DB");
@@ -23,7 +24,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
 });
-// LigLiIO3sBVL94zI
+// LigLiIO3sBVL94zI - MongoDB PW, username: admin
 app.post('/api/posts',(req, res, next) => {
   const post = new Post({
     title: req.body.title,
@@ -35,23 +36,20 @@ app.post('/api/posts',(req, res, next) => {
   })
 });
 
-app.use('/api/posts',(req, res, next) =>{
-  const posts = [
-    {
-      id: 'dadf121212',
-      title: 'First serverside stuff',
-      content: 'Coming from the server!'
-    },
-    {
-      id: 'asdfasdf',
-      title: 'Second serverside stuff',
-      content: 'Coming from the server!!'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
+app.get('/api/posts',(req, res, next) =>{
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched successfully!',
+      posts: documents
+    });
   });
 });
+
+app.delete('/api/posts/:id', (req, res, next)=> {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({message: 'Post Deleted!'});
+  });
+})
 
 module.exports = app;
