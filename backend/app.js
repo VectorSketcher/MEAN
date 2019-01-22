cd822d0810b841cd520f95ca7510696f8d20fea9
vectorsketcher <westboarderm@gmail.com>
2019-01-22 14:09:51 -0700

    hooked up mongDB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb://admin:LigLiIO3sBVL94zI@cluster0-shard-00-00-h8mtn.mongodb.net:27017,cluster0-shard-00-01-h8mtn.mongodb.net:27017,cluster0-shard-00-02-h8mtn.mongodb.net:27017/node-angular?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true")
+.then(() => {
+  console.log("Connected to Mongo DB");
+})
+.catch(()=>{
+  console.log("Connection Failed!");
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -12,10 +23,13 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
 });
-
+// LigLiIO3sBVL94zI
 app.post('/api/posts',(req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   })
